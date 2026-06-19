715a7915b75ae78589ea74cc93f9ce892f234daf
white-venom <kansalsujeetdps@gmail.com>
2026-06-19 13:59:56 +0530

    style: rename Portal/Account Name label to Portal Name in LedgerReportView

--- frontend/src/app/components/LedgerReportView.tsx
+++ frontend/src/app/components/LedgerReportView.tsx
@@ -707,7 +707,7 @@ ${publicLink ? `\nView Full Ledger: ${publicLink}` : ""}`;
                   <span className="text-[10px] font-black text-indigo-400 dark:text-indigo-550 uppercase tracking-widest block">Transfer Target Account</span>
                   {selectedEntryForDetails.portal_name && (
                     <div className="flex justify-between border-b border-indigo-100/20 dark:border-indigo-900/10 pb-1">
-                      <span>Portal/Account Name</span>
+                      <span>Portal Name</span>
                       <span className="font-extrabold uppercase">{selectedEntryForDetails.portal_name}</span>
                     </div>
                   )}
